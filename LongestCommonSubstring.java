2f09e8e575ad7988579e50dff2ecd9886b717096
vatsaljrathod <vatsaljrathod@gmail.com>
2016-07-31 12:55:01 +0530

    Update LongestCommonSubstring.java

--- LongestCommonSubstring.java
+++ LongestCommonSubstring.java
@@ -18,18 +18,20 @@ public class LongestCommonSubstring {
         String s2= sc.nextLine();
         
         int[][] mat = new int[s1.length()+1][s2.length()+1];
-        
+        int maxLength=0;
         for(int i=1;i<=s1.length();i++){
             for(int j=1;j<=s2.length();j++){
                 
-                if(s1.charAt(i-1)==s2.charAt(j-1))
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
                     mat[i][j]=1+mat[i-1][j-1];
-                else
-                    mat[i][j]=Math.max(mat[i-1][j],mat[i][j-1]);
+                
+                    if(maxLength<mat[i][j])
+                        maxLength=mat[i][j];
+                }                
                 
             }
         }
         
-        System.out.println(mat[s1.length()][s2.length()]);
+        System.out.println(maxLength);
     }
 }
